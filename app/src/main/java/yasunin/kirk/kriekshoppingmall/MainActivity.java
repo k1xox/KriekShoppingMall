4b872290c08a58a44184236c8c2463f6767d72c7
kamonchanok <primkamonchanok@gmail.com>
2018-03-06 13:12:39 +0700

    บ่าย

--- app/src/main/java/yasunin/kirk/kriekshoppingmall/MainActivity.java
+++ app/src/main/java/yasunin/kirk/kriekshoppingmall/MainActivity.java
@@ -3,11 +3,20 @@ package yasunin.kirk.kriekshoppingmall;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import yasunin.kirk.kriekshoppingmall.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        if ( savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentMainFragment,new MainFragment()).commit();
+        }
+
+
+    }//Main Mehod
+}//Main Class
---
+++ app/src/main/java/yasunin/kirk/kriekshoppingmall/fragment/MainFragment.java
@@ -0,0 +1,33 @@
+package yasunin.kirk.kriekshoppingmall.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import yasunin.kirk.kriekshoppingmall.R;
+
+/**
+ * Created by User on 6/3/2561.
+ */
+
+public class MainFragment  extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+
+    }//Main Method
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fraingment_main,container,false);
+        return view;
+    }
+}
