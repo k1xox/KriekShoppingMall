da4a84694a75164060764e10336bbcc74fd74ae0
kamonchanok <primkamonchanok@gmail.com>
2018-03-06 14:31:45 +0700

    Create Icon Menu on Toolbar Finish

--- app/src/main/java/yasunin/kirk/kriekshoppingmall/fragment/RegisterFragment.java
+++ app/src/main/java/yasunin/kirk/kriekshoppingmall/fragment/RegisterFragment.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,14 +27,54 @@ public class RegisterFragment extends Fragment {
         //creat toolbar
         creatToolbar();
 
+    }//Main Method
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId()== R.id.itemUpload){
+            uploadToServer();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void uploadToServer() {
+
+
+
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+
+        inflater.inflate(R.menu.menu_register,menu);
+
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     private void creatToolbar() {
+
+        setHasOptionsMenu(true);
+
         Toolbar toolbar = getView().findViewById(R.id.ToolbarRegister);
         ((MainActivity)getActivity()).setSupportActionBar(toolbar);
 
         ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.register));
         ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.message_have_space1));
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                getActivity().getSupportFragmentManager().popBackStack();
+
+            }
+        });
+
     }
 
     @Nullable
