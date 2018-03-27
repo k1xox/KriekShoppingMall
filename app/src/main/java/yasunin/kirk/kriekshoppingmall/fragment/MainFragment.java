58aeb29d76cdfb1a85c73587a6cc853be117c1a4
kamonchanok <primkamonchanok@gmail.com>
2018-03-27 14:44:38 +0700

    Authen Ok

--- app/src/main/java/yasunin/kirk/kriekshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/yasunin/kirk/kriekshoppingmall/fragment/MainFragment.java
@@ -3,16 +3,23 @@ package yasunin.kirk.kriekshoppingmall.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import yasunin.kirk.kriekshoppingmall.R;
+import yasunin.kirk.kriekshoppingmall.utility.GetAllDataByURL;
 import yasunin.kirk.kriekshoppingmall.utility.MyAlert;
+import yasunin.kirk.kriekshoppingmall.utility.MyConstant;
 
 /**
  * Created by User on 6/3/2561.
@@ -49,7 +56,58 @@ public class MainFragment  extends Fragment{
                             getString(R.string.message_have_space1));
                 }else {
 //                    No Space
+                try {
 
+                    MyConstant myConstant = new MyConstant();
+                    boolean statusAboolean = true;
+                    GetAllDataByURL getAllDataByURL = new GetAllDataByURL(getActivity());
+                    getAllDataByURL.execute(myConstant.getUrlGetAllUserString());
+
+                    String jsonString = getAllDataByURL.get();
+                    Log.d("27MarchV1","JSON ==> " + jsonString);
+
+                    JSONArray jsonArray = new JSONArray(jsonString);
+
+                    String[] columnStrings = myConstant.getColumnUser();
+                    String[] loginStrings = new String[columnStrings.length];
+
+                    for (int i=0; i<jsonArray.length();)i+=1){
+
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
+                        if (userString.equals(jsonObject.getString(columnStrings[2]))) {
+
+                            statusAboolean = false;
+                            for (int i1=0; i1<columnStrings.length; i1+=1) {
+                                loginStrings[i1] = jsonObject.getString(columnStrings[i1]);
+
+                            }
+
+
+                        }//if
+
+                    }//for
+
+                    if (statusAboolean) {
+//                        User False
+                        MyAlert myAlert = new MyAlert(getActivity());
+                        myAlert.myDialog("User False",
+                                "No this User in my Database");
+                    }else if (passwordString.equals(loginStrings[3])) {
+//                        Password True
+                        Toast.makeText(getActivity(),"Welcome" + loginStrings[1],
+                                Toast.LENGTH_SHORT).show();
+                    }else {
+                        MyAlert myAlert = new MyAlert(getActivity());
+                        myAlert.myDialog("Password False",
+                                "Please Try Again Password False");
+                    }
+
+
+
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
 
 
                 }//if
