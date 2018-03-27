fc750796cd7e0798d505f088f4d138c9fe02f5f1
kamonchanok <primkamonchanok@gmail.com>
2018-03-27 10:30:32 +0700

    Start 27 Match

--- app/src/main/java/yasunin/kirk/kriekshoppingmall/fragment/MainFragment.java
+++ app/src/main/java/yasunin/kirk/kriekshoppingmall/fragment/MainFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+
 import yasunin.kirk.kriekshoppingmall.R;
 
 /**
---
+++ app/src/main/java/yasunin/kirk/kriekshoppingmall/utility/MyConstant.java
@@ -0,0 +1,19 @@
+package yasunin.kirk.kriekshoppingmall.utility;
+
+/**
+ * Created by User on 27/3/2561.
+ */
+
+public class MyConstant {
+
+
+    //about url
+private String urlAddUserString = "http://androidthai.in.th/kir/addDataUng.php";
+
+    public String getUrlAddUserString() {
+        return urlAddUserString;
+    }
+
+
+//Main class
+}
